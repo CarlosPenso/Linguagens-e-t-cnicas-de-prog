0eee3fbd716abad5015aab6a645afefdee26abf4
Carlos Henrique Penso <carlos_penso@hotmail.com>
2026-09-19 16:47:49 -0300

    Replace currency conversion with distance calculation

--- Exercícios lista/Lista 1/Lista 1 - Exercício 8.c
+++ Exercícios lista/Lista 1/Lista 1 - Exercício 8.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main(int argc, char *argv[]) {
     
-    float reais, cotacao, dolares;
+    float x1, y1, x2, y2, distancia;
 
-    printf("Digite o valor em reais: R$ ");
-    scanf("%f", &reais);
+    printf("Digite x1 e y1: ");
+    scanf("%f %f", &x1, &y1);
 
-    printf("Digite a cotacao do dolar: R$ ");
-    scanf("%f", &cotacao);
+    printf("Digite x2 e y2: ");
+    scanf("%f %f", &x2, &y2);
 
-    dolares = reais / cotacao;
+    distancia = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 
-    printf("Valor em dolares: US$ %.2f\n", dolares);
+    printf("DISTANCIA = %.4f\n", distancia);
 
     return 0;
 }
